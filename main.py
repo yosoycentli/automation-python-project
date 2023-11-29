306f419ca7c20792fdf514b4e706e75846358928
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2023-11-28 19:31:22 -0600

    [ADD] logic to see how many products we have from each supplier

---
+++ main.py
@@ -0,0 +1,17 @@
+import openpyxl
+
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
+
+products_per_supplier = {}
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier.get(supplier_name)
+        products_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        products_per_supplier[supplier_name] = 1
+
+print(products_per_supplier)
